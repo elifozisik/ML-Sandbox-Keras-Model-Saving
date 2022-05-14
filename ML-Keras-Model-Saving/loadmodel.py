c3450e397fdc62ac86e1754952716a34617e7bed
elifozisik <51250917+elifozisik@users.noreply.github.com>
2022-05-15 02:21:08 +0300

    Update loadmodel.py

--- ML-Keras-Model-Saving/loadmodel.py
+++ ML-Keras-Model-Saving/loadmodel.py
@@ -6,12 +6,12 @@ from tensorflow import keras
 # It can be used to reconstruct the model identically.
 reconstructed_model = keras.models.load_model("my_model")
 
-test_input = np.random.random((128, 32))
+"""test_input = np.random.random((128, 32))
 test_target = np.random.random((128, 1))
 
 # The reconstructed model is already compiled and has retained the optimizer
 # state, so training can resume:
-reconstructed_model.fit(test_input, test_target)
+reconstructed_model.fit(test_input, test_target)"""
 
 testx = np.random.random((10, 32))
 testy = np.random.random((10, 1))
